8887f720a60a11beff4fcc3a467444f39a589649
dekguh <kdkteguhmahesa@gmail.com>
2023-02-21 13:52:13 +0800

    chore: add leaflet to dom and install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 function App() {
+  const position = [51.505, -0.09]
+  const [map, setMap] = useState(null)
+
+  console.log({ map })
+
   return (
-    <div>
-         tes
+    <div className='h-[100vh]'>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        whenReady={(event) => setMap(event.target)}
+        className='h-full'
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   )
 }
